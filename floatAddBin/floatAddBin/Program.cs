77441b05e65c99263ff21515e22b837b0f287a35
jayashanker.padishala <jayas9896@gmail.com>
2017-07-24 19:09:07 +0530

    FloatAddition
    Addition of 2 float numbers in binary and converting back to float.

---
+++ floatAddBin/floatAddBin/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace floatAddBin
+{
+    class myMath
+    {
+        public static int max = 0;
+        public static void intToBin(int num, int[] bin)
+        {
+            int dup = num, i = 0, j = 0;
+            for (i = 0; dup > 0; i++)
+            {
+                bin[i] = dup % 2;
+                dup /= 2;
+            }
+            if (i > max)
+                max = i;
+        }
+        public static void decToBin(double num, int[] bin)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                bin[i] = 0;
+            }
+            for (int i = 14; i >= 0; i--)
+            {
+                bin[i] = (int)(Math.Floor(num * 2));
+                num = (num * 2) - (int)(num * 2);
+            }
+        }
+        public static void joinIntDec(int[] a, int[] aI, int[] aD)
+        {
+            bool print = false;
+            for (int i = 15; i < max + 15; i++)
+            {
+                a[i] = aI[i - 15];
+            }
+            for (int j = max + 15; j >= 15; j--)
+            {
+                if (a[j] == 1)
+                    print = true;
+                if (print == true)
+                    Console.Write(a[j]);
+            }
+            Console.Write('.');
+            for (int i = 14; i >= 0; i--)
+            {
+                a[i] = aD[i];
+                Console.Write(a[i]);
+            }
+            Console.WriteLine();
+        }
+        public static void readNum(out double num, out int numInt, out double numDec)
+        {
+            Console.Write("Enter the number: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            numInt = Convert.ToInt32(Math.Floor(num));
+            numDec = num - Convert.ToDouble(numInt);
+        }
+        public static double sumBinTot(int[] a, int[] b, int max)
+        {
+            int i = 0;
+            double sum = 0;
+            bool print = false;
+            int[] sumIntTot = new int[100];
+            for (int j = 0; j < 100; j++)
+                sumIntTot[j] = -1;
+            for (i = 0; i < max + 3; i++)
+            {
+                if ((i > max) && (sumIntTot[i] == -1))
+                    break;
+                sumIntTot[i] += a[i] + b[i] + 1;
+                if (sumIntTot[i] > 1)
+                {
+                    sumIntTot[i] = sumIntTot[i] % 2;
+                    sumIntTot[i + 1] = sumIntTot[i] / 2;
+                }
+            }
+            Console.Write("Total sum of Num in binary is ");
+            while (i >= 0)
+            {
+                if (sumIntTot[i] == 1)
+                {
+                    print = true;
+                    if (i > max)
+                        max = i;
+                }
+                if (print == true)
+                {
+                    Console.Write(sumIntTot[i]);
+                    if (i == 15)
+                        Console.Write('.');
+                    sum += (Math.Pow(2, i - 15)) * sumIntTot[i];
+                }
+                i--;
+            }
+            Console.WriteLine();
+            return sum;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region initialization
+            double num1, num2;
+            int numInt1, numInt2;
+            double numDec1, numDec2;
+            int[] aI = new int[85];
+            for (int k = 0; k < 85; k++)
+            {
+                aI[k] = 0;
+            }
+            int[] bI = new int[85];
+            for (int k = 0; k < 85; k++)
+            {
+                bI[k] = 0;
+            }
+            int[] aD = new int[15];
+            int[] bD = new int[15];
+            int[] a = new int[100];
+            int[] b = new int[100];
+            #endregion
+            myMath.readNum(num: out num1, numInt: out numInt1, numDec: out numDec1);
+            myMath.readNum(num: out num2, numInt: out numInt2, numDec: out numDec2);
+            myMath.intToBin(Math.Abs(numInt1), aI);
+            myMath.decToBin(numDec1, aD);
+            myMath.intToBin(Math.Abs(numInt2), bI);
+            myMath.decToBin(numDec2, bD);
+            int max = myMath.max;
+            Console.Write("Num1 in binary is \t\t");
+            myMath.joinIntDec(a, aI, aD);
+            Console.Write("Num2 in binary is \t\t");
+            myMath.joinIntDec(b, bI, bD);
+            Console.WriteLine("Total sum in int is  " + myMath.sumBinTot(a, b, max + 15));
+        }
+    }
+}
---
+++ floatBinAddInt/floatBinAddInt/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace floatBinAddInt
+{
+    class myMath
+    {
+        public static int max = 0, dotPos = 0;
+        public static long intToBin(int intNumber)
+        {
+            long reverseBinary, binary;
+            int count = 0;
+            for (reverseBinary = 0; intNumber > 0; intNumber /= 2)
+            {
+                if ((reverseBinary == 0) && (intNumber % 2 == 0))
+                {
+                    count++;
+                }
+                reverseBinary = (reverseBinary * 10) + intNumber % 2;
+            }
+            for (binary = 0; reverseBinary > 0; reverseBinary /= 10)
+            {
+                binary = binary * 10 + (reverseBinary % 10);
+            }
+            for (int i = 0; i < count; i++)
+            {
+                binary *= 10;
+            }
+            return binary;
+        }
+        public static decimal decToBin(double a)
+        {
+            decimal x = 0;
+            int i = 0;
+            for (i = 0; i < 14; i++)
+            {
+                if (a == 0)
+                    break;
+                x = (x * 10) + ((int)(a * 2));
+                a = (a * 2) - (int)(a * 2);
+            }
+            if (i > max)
+                max = i;
+            decimal m = 1;
+            for (int j = 0; j < max; j++)
+            {
+                m *= 10;
+            }
+            return (x / m);
+        }
+        public static int[] Correct(decimal num)
+        {
+            string str = num.ToString();
+            int j = str.Length - 2;
+            int[] Num = new int[str.Length + 1];
+            foreach (char i in str)
+            {
+                if (j < 0)
+                    break;
+                if (i != '.')
+                    Num[j] = i - 48;
+                if (i == '.')
+                {
+                    dotPos = j;
+                    j++;
+                }
+                j--;
+            }
+            for (int i = 0; i <= str.Length; i++)
+            {
+                if (Num[i] > 1)
+                {
+                    Num[i + 1] += (Num[i] / 2);
+                    Num[i] %= 2;
+                }
+            }
+            return Num;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            Int64 xIb = myMath.intToBin((int)x);
+            Int64 yIb = myMath.intToBin((int)y);
+            decimal xDb = myMath.decToBin(x - (int)x);
+            decimal yDb = myMath.decToBin(y - (int)y);
+            decimal xB = xIb + xDb;
+            decimal yB = yIb + yDb;
+            Console.WriteLine(x + " in bin is " + xB);
+            Console.WriteLine(y + " in bin is " + yB);
+            decimal sum = xB + yB;
+            Console.Write("Sum in Binary is ");
+            int[] Num = myMath.Correct(sum);
+            int k = 0;
+            double Sum = 0;
+            bool print = false;
+            for (int i = Num.Length - 1; i >= 0; i--)
+            {
+                Sum += Num[i] * Math.Pow(2, i - myMath.dotPos - 1);
+                if (Num[i] == 1)
+                    print = true;
+                if (print == true)
+                {
+                    if (k == Num.Length - myMath.dotPos - 1)
+                        Console.Write('.');
+                    Console.Write(Num[i]);
+                }
+                k++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sum = " + Sum);
+        }
+    }
+}
